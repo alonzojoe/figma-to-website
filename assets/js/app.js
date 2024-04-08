5460a86bb2705e7441aa27261dad2e2735e036b1
Joenell Canlas Alonzo <medusazxc123@gmail.com>
2024-04-09 00:01:03 +0800

    Make navbar fixed then calcl width and set margin top to the first section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,6 @@
 console.log('figmaa to website')
+
+const headerHeight = document.querySelector('header').offsetHeight;
+document.querySelector('.home').style.marginTop = `${headerHeight}px`
+
+console.log('header', headerHeight)
